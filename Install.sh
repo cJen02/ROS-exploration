f41bd4a0bf44835a95a04b11c4a0d7b5b97a11f9
cJen02 <86952828+cJen02@users.noreply.github.com>
2023-09-02 22:42:44 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -10,3 +10,10 @@ source /opt/ros/noetic/setup.bash
 
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
+
+
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+
+sudo apt install python3-rosdep
+sudo rosdep init
+rosdep update
