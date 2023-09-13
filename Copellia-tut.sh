4177320c8be0aa55cd5591dbeba52fa3c6039740
Jenish Chauhan <86952828+cJen02@users.noreply.github.com>
2023-09-14 00:51:12 +0500

    Update Copellia-tut.sh

--- Copellia-tut.sh
+++ Copellia-tut.sh
@@ -3,4 +3,9 @@
 cp     CoppeliaSim_Edu_V4_1_0_Ubuntu16_04/programming/remoteApiBindings/python/python/*                       Project/api/
 cp     CoppeliaSim_Edu_V4_1_0_Ubuntu16_04/programming/remoteApiBindings/lib/lib/Ubuntu18_04/remoteApi.so      Project/api/remoteApi.so
 
-# copy 'simRemoteApi.start(19999)' in copellia 
+# copy [Project/api/simpleTest.py/#Line5] 'simRemoteApi.start(19999)' in  [copellia/Floor/Add/Associated child script/Threaded/Lua] function coroutineMain()
+
+""
+
+""
+# start coppeliasim simulation & run test_connection.py
